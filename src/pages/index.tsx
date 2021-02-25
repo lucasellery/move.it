f3f495db3a4329124373f083059da52c6bfc9d98
lucasellery <lucas.ellery@somapay.com.br>
2021-02-25 17:07:20 -0300

    (feature) challenge box started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,14 @@ import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
-import Head from 'next/head';
+import Head from "next/head";
 
-import styles from '../styles/pages/Home.module.css';
+import styles from "../styles/pages/Home.module.css";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-
       <Head>
         <title>Início | move.it</title>
       </Head>
@@ -26,7 +26,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
